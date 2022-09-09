0e8a3d8bb84638fe98e4aa8fcdcf7ed48a229afe
Oleg <hellreebok@gmail.com>
2022-09-09 16:08:05 +0300

    Add basename

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/Kapusta/">
+      <BrowserRouter basename="/finance-manager/">
         <App />
       </BrowserRouter>
     </Provider>
