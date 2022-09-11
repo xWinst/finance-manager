205e38790ab43dc8c3c2aceca985349649386f11
Oleg <hellreebok@gmail.com>
2022-09-11 22:03:38 +0300

    fixed bugs3

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,61 +1,61 @@
-import { createSlice } from '@reduxjs/toolkit';
-import message from 'helpers/Message';
-import { register, logIn, googleAuth, logOut, refresh } from './operations';
-// import {  } from './actions';
+// import { createSlice } from '@reduxjs/toolkit';
+// import message from 'helpers/Message';
+// import { register, logIn, googleAuth, logOut, refresh } from './operations';
+// // import {  } from './actions';
 
-const initalState = {
-    email: null,
-    id: null,
-    isLoading: false,
-    error: null,
-    isLoggedIn: false,
-    accessToken: null,
-    refreshToken: null,
-    sid: null,
-    userData: null,
-};
+// const initalState = {
+//     email: null,
+//     id: null,
+//     isLoading: false,
+//     error: null,
+//     isLoggedIn: false,
+//     accessToken: null,
+//     refreshToken: null,
+//     sid: null,
+//     userData: null,
+// };
 
-const authSlice = createSlice({
-    name: 'auth',
-    initalState,
-    extraReducers: {
-        [register.pending]: (state, action) => {
-            console.log('isLoading: ', state.isLoading);
-            state.isLoading = true;
-            state.error = '';
-        },
+// const authSlice = createSlice({
+//     name: 'auth',
+//     initalState,
+//     extraReducers: {
+//         [register.pending]: (state, action) => {
+//             console.log('isLoading: ', state.isLoading);
+//             state.isLoading = true;
+//             state.error = '';
+//         },
 
-        [register.fulfilled]: (state, action) => {
-            console.log('action F: ', action);
-            state.email = action.payload.email;
-            state.id = action.payload.id;
-            state.isLoggedIn = true;
-        },
+//         [register.fulfilled]: (state, action) => {
+//             console.log('action F: ', action);
+//             state.email = action.payload.email;
+//             state.id = action.payload.id;
+//             state.isLoggedIn = true;
+//         },
 
-        [register.rejected]: (state, action) => {
-            console.log('action R: ', action);
-            message.error('Registration error', `${action.error}`, 'Ok');
-        },
+//         [register.rejected]: (state, action) => {
+//             console.log('action R: ', action);
+//             message.error('Registration error', `${action.error}`, 'Ok');
+//         },
 
-        [logIn.pending]: (state, action) => {
-            state.isLoading = true;
-            state.error = '';
-        },
+//         [logIn.pending]: (state, action) => {
+//             state.isLoading = true;
+//             state.error = '';
+//         },
 
-        [logIn.fulfilled]: (state, action) => {
-            console.log('action LF: ', action);
-            state.accessToken = action.payload.accessToken;
-            state.refreshToken = action.payload.refreshToken;
-            state.sid = action.payload.sid;
-            state.userData = action.payload.userData;
-            state.isLoggedIn = true;
-        },
+//         [logIn.fulfilled]: (state, action) => {
+//             console.log('action LF: ', action);
+//             state.accessToken = action.payload.accessToken;
+//             state.refreshToken = action.payload.refreshToken;
+//             state.sid = action.payload.sid;
+//             state.userData = action.payload.userData;
+//             state.isLoggedIn = true;
+//         },
 
-        [logIn.rejected]: (state, action) => {
-            console.log('action LR: ', action);
-            message.error('LogIn error', `${action.payload.message}`, 'Ok');
-        },
-    },
-});
+//         [logIn.rejected]: (state, action) => {
+//             console.log('action LR: ', action);
+//             message.error('LogIn error', `${action.payload.message}`, 'Ok');
+//         },
+//     },
+// });
 
-export default authSlice.reducer;
+// export default authSlice.reducer;
