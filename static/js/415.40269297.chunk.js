9254deb454cd7de1be42076a9b88fe6e63e73203
xWinst <36066519+xWinst@users.noreply.github.com>
2022-09-16 21:33:20 +0000

    Deploying to gh-pages from @ xWinst/finance-manager@f58efff321252eb0f3a256c0d5fa4000647e6b19 🚀

---
+++ static/js/415.40269297.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfinance_manager=self.webpackChunkfinance_manager||[]).push([[415],{4415:function(n,e,a){a.r(e);var c=a(184);e.default=function(){return(0,c.jsx)("div",{children:" Income"})}}}]);
+//# sourceMappingURL=415.40269297.chunk.js.map
